65ce958001d94d7ca779668a448c8fca27dafc3f
Varha Sabolch <vargha.sabolch@gmail.com>
2022-10-07 18:29:11 +0200

    user create & hash

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import bcrypt from "bcrypt"
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/user.create.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(private userRepository: Repository<User>) {
+    }
+
+    async create(createDTO: CreateUserDto) {
+
+        if (createDTO.password !== createDTO.confirmPassword) {
+            throw new BadRequestException("Confirm password not match")
+        }
+
+
+        const user = await this.userRepository.find({
+            where: [
+                { email: createDTO.email },
+                { username: createDTO.username }
+            ]
+        })
+
+
+        if (!user) {
+            throw new BadRequestException("Email or username already taken!")
+        }
+
+        /**
+         * This regex will enforce these rules:
+            At least one upper case English letter
+            At least one lower case English letter
+            At least one digit
+            At least one special character
+            Minimum eight in length
+         */
+        if (!/^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/.test(createDTO.password)) {
+            throw new BadRequestException("Password doesnt match the pattern!")
+        }
+
+        const hashedPassword = await bcrypt.hash(createDTO.password, 10);
+
+        const newUser = this.userRepository.create({
+            email: createDTO.email,
+            password: hashedPassword,
+            username: createDTO.username,
+        })
+
+        return this.userRepository.save(newUser);
+    }
+
+
+    findByEmailOrUsername(email?: string, username?: string) {
+        return this.userRepository.find({
+            where: [
+                { email: email },
+                { username: username }
+            ]
+        })
+    }
+}
